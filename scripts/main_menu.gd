6dc42d1df0af6e9312a96a48d7c0c6b163ad78ff
Krystof Klestil <krystofklestil@live.com>
2019-01-10 10:42:08 +0100

    added button and font for settings

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var config_file_path : String = "user://config.cfg"
+var resizing : bool = false
 
 onready var map_list : ItemList = $SidePanelSpace/SidePanel/MapList
 
@@ -10,20 +11,22 @@ const MAX_MAP_LIST_SIZE = 340
 func _ready():
 	set_process_input(false)
 	map_list.rect_min_size.y = load_setting("main_menu", "map_list_length", 139)
+	map_list.select(0)
 
 func _input(event):
 	if event is InputEventMouseButton:
 		#	show mouse and stop listening to input when LMB is unclicked
 		if event.button_index == 1 and !event.pressed:
+			set_process_input(false)
 			#	save the length of the box into our config file
 			save_setting("main_menu", "map_list_length", map_list.rect_min_size.y)
-			
 			Input.set_mouse_mode(0)
 			var new_mouse_pos = $SidePanelSpace/SidePanel/grab_container/GrabMe.rect_global_position
 			new_mouse_pos.x += $SidePanelSpace/SidePanel/grab_container/GrabMe.rect_size.x / 2
 			new_mouse_pos.y += $SidePanelSpace/SidePanel/grab_container/GrabMe.rect_size.y / 2			
 			Input.warp_mouse_position(new_mouse_pos)
-			set_process_input(false)
+			resizing = false
+			
 
 	elif event is InputEventMouseMotion:
 	#	listen to mouse speed and apply it to the rectangle size
@@ -32,6 +35,7 @@ func _input(event):
 		if map_list.rect_min_size.y + change > MIN_MAP_LIST_SIZE and map_list.rect_min_size.y + change < MAX_MAP_LIST_SIZE:
 			map_list.rect_min_size.y += change
 			$SidePanelSpace/SidePanel.queue_sort()
+			play_knob_turn()
 
 func load_setting(section, key, default_value):
 	var conf = ConfigFile.new()
@@ -43,15 +47,19 @@ func load_setting(section, key, default_value):
 	else:
 		return default_value
 	
-func save_setting(section, key, value):
+func save_setting(section, key, value) -> void:
 	var conf = ConfigFile.new()
 	var err = conf.load(config_file_path)
 	conf.set_value(section, key, value)
 	conf.save(config_file_path)
 
-func _on_GrabMe_pressed():
+func _on_GrabMe_pressed() -> void:
+	resizing = true
 	set_process_input(true)
 	Input.set_mouse_mode(2)
 
 func play_click() -> void:
-	$click.play()
+	$audio/click.play()
+	
+func play_knob_turn() -> void:
+	$audio/knob.play()
