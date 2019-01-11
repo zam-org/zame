a02f75964dacc3dc1f86aa5d317184c8abe3b923
Krystof Klestil <krystofklestil@live.com>
2019-01-11 21:09:48 +0100

    toolset more complete added the ability to center player incase they fly off

--- scripts/game.gd
+++ scripts/game.gd
@@ -11,3 +11,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_CenterPlayer6_pressed():
+	$character.motion = Vector2()
+	$character.position = $Camera.position
