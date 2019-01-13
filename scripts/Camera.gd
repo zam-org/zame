d5629bd533cbefa262d8e1a81bc25fc96f464a8b
Krystof Klestil <krytofklestil@live.com>
2019-01-13 20:32:14 +0100

    camera shake - for now added when the exit charges

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -5,17 +5,20 @@ var smooth_mouse : Vector2
 
 var pan_multiplier : int = 8
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var shaking : bool = false
+var shaking_multiplier : float = 1.0
+var shake_amount : float = 0
+
 func _process(delta):
 	smooth_mouse = smooth_mouse.linear_interpolate(mouse_speed, 50 * delta)
 	
 	if Input.is_mouse_button_pressed(3):
 		self.position -= smooth_mouse
 	
-	if Input.is_mouse_button_pressed(4):
-		self.position.x += 1
-	
 	mouse_speed = Vector2()
+	
+	if shaking:
+		self.offset = Vector2(rand_range(-shake_amount, shake_amount), rand_range(-shake_amount, shake_amount))
 		
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -23,3 +26,15 @@ func _input(event):
 	
 	elif event is InputEventPanGesture:
 		self.position += event.delta * pan_multiplier
+		
+#	shake it baby! set the strength and duration of the shake and watch the camera go wild
+func shake(strength : float = 5.0, duration : float = 2.0):
+	shaking = true
+	shake_amount = strength
+	$Tween.interpolate_property(self, 'shake_amount', shake_amount, 0, duration, Tween.TRANS_CUBIC, Tween.EASE_OUT, 0)
+	$Tween.start()
+	yield($Tween, 'tween_completed')
+	shaking = false
+
+func _on_Finish_activated():
+	shake(10.5, 2.2)
